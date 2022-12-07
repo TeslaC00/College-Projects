1540017c3dca61b0ebf8080d3bf5a806d7c54e51
TeslaC00 <111948419+TeslaC00@users.noreply.github.com>
2022-12-07 19:05:16 +0530

    modified main

--- main.c
+++ main.c
@@ -1,7 +1,31 @@
 #include<stdio.h>
+#include<stdlib.h>
+void list(void);
+void issue(void);
+void rm(void);
 int main(){
 
-    int i;
+    while(1){
+        int i;
+        system("cls");
+        printf("Library Management System\n\n");
+        printf("0.Exit\n1.List\n2.Issue book\n3.Remove book\n");
+        printf("Enter your choice: ");
+        scanf("%d",&i);
+        switch(i){
+            case 0: exit(0); break;
+            case 1: list(); break;
+            case 2: issue(); break;
+            case 3: rm(); break;
+        }
+
+    }
 
     return 0;
 }
+
+void list(){}
+
+void issue(){}
+
+void rm(){}
